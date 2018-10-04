8f9d24506249985988321397128d634852da2104
Levi Porter <levi@levibrooke.com>
2018-10-04 11:19:05 -0700

    Add react helmet to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import '../sass/main.scss';
 
-const Layout = ({children}) => <div>{children}</div>
+const Layout = ({children}) => [
+  <Helmet key="app-head">
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width, user-scalable=no" />
+  </Helmet>,
+  <div key="app-content">
+    {children}
+  </div>
+];
 
 export default Layout;
